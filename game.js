93cf21aa24f6927ab67ed16774f60bacde472893
Paul-Nixon <nixonp4@gmail.com>
2021-03-08 19:01:45 -0600

    modified game.html; commented-out code in game.js is already-built timer code

--- game.js
+++ game.js
@@ -9,5 +9,50 @@ else
 
 function ready()
 {
-    
+    document.querySelector(".player-choice").addEventListener("changed", answerChanged);
 }
+
+
+function answerChanged(event)
+{
+
+}
+
+/*function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    
+    return {
+      total,
+      minutes,
+      seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector(id);
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  const timeInMinutes = 10;
+  const currentTime = Date.parse(new Date());
+  const deadline = new Date(currentTime + timeInMinutes*60*1000);
+  initializeClock('.timer', deadline);
+  
+  
+  
